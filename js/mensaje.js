7b04b2765123ee6439fe30a6eb74b77f838ea4c4
Laura <laurabeltranp15@gmail.com>
2023-10-25 12:40:23 +0200

    añadido js recarga

--- js/mensaje.js
+++ js/mensaje.js
@@ -1,15 +1,31 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const contactForm = document.getElementById("contactForm");
-    const mensajeExito = document.getElementById("mensajeExito");
+// Obtén el formulario y el elemento de mensaje de éxito
+const form = document.querySelector('form');
+const mensajeExito = document.getElementById('mensajeExito');
 
-    contactForm.addEventListener("submit", function(event) {
-        event.preventDefault(); // Evitar que el formulario se envíe de manera predeterminada
+// Agrega un evento de envío al formulario
+form.addEventListener('submit', async (event) => {
+    event.preventDefault(); // Evita el envío predeterminado del formulario
 
-        // Aquí agregarías la lógica para enviar el formulario al servidor si es necesario
+    // Envía el formulario utilizando Fetch API o XMLHttpRequest
+    try {
+        const response = await fetch(form.action, {
+            method: form.method,
+            body: new FormData(form),
+        });
 
-        // Mostrar el mensaje de éxito
-        mensajeExito.style.display = "block";
-
-        // Puedes redirigir al usuario a una página de confirmación o realizar otras acciones aquí
-    });
+        if (response.ok) {
+            // El formulario se procesó correctamente
+            mensajeExito.style.display = 'block'; // Muestra el mensaje de éxito
+            form.reset(); // Limpia el formulario si es necesario
+            // Redirige a la página de inicio después de un cierto tiempo
+            setTimeout(() => {
+                window.location.href = form.querySelector('input[name="_next"]').value;
+            }, 3000); // Redirige después de 3 segundos (puedes ajustar el tiempo)
+        } else {
+            // Maneja el caso en el que el formulario no se procesó correctamente
+            console.error('Error al procesar el formulario');
+        }
+    } catch (error) {
+        console.error('Error de red:', error);
+    }
 });
